ce5c2efbf443cdc2a0a7617036884950dc931c81
pratikg88 <pratikgujarathi88@gmail.com>
2018-02-05 15:10:13 +0530

    Update hq_online.js

--- hq_online.js
+++ hq_online.js
@@ -10,7 +10,7 @@ const Discord = require('discord.js');
 // Create an instance of a Discord client
 const client = new Discord.Client();
 
-
+const newUsers = [];
 
 // The ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted
@@ -28,28 +28,38 @@ client.on('message', message => {
 	  // If the message is "!nap"
   if (message.content === '!nap') {
     
-    message.channel.send(' Do not attack Lords they are Friendly \n Do not attack Hive because they are Murphys babies :stuck_out_tongue_winking_eye:');
+    message.channel.send('\n Do not attack Lords they are Friendly \n Do not attack Hive because they are Murphys babies :stuck_out_tongue_winking_eye: \n We have temp NAP with Torch');
 	 }
 	  // If the message is "!war"
   if (message.content === '!war') {
     
-    message.channel.send('Burn the Torch :fire:');
+    message.channel.send('Check !nap before hitting anyone. Make a good choices :fire:');
 	}
 	  // If the message is "!help"
   if (message.content === '!help') {
     
-    message.channel.send(' !R \n !nap \n !war');
+    message.channel.send('\n !R \n !nap \n !war');
 
   }
 });
-// Create an event listener for new guild members
-client.on('guildMemberAdd', member => {
-  // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.find('name', 'member-log');
-  // Do nothing if the channel wasn't found on this server
-  if (!channel) return;
-  // Send the message, mentioning the member
-  channel.send(`Welcome to the alliance, ${member}`);
+
+//Welcome new member
+client.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
+  newUsers[guild.id].set(member.id, member.user);
+
+  if (newUsers[guild.id].size > 1) {
+    const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
+    guild.channels.get(guild.id).send("Welcome to our alliance \n" + userlist);
+    newUsers[guild.id].clear();
+  }
 });
+
+client.on("guildMemberRemove", (member) => {
+  const guild = member.guild;
+  if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
+});
+
 // Log our bot in
 client.login(process.env.BOT_TOKEN);
